5351171d3aaa0765c76c2799467f98fb57aa6381
YaliZhang1 <yali.zhang@hyperisland.se>
2025-01-30 22:16:20 +0100

    deply

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,44 @@
-import React, { Component } from "react";
-import { Route, Routes } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import MyNavLink from "./components/MyNavLink";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <div className="row">
-          {/*offset-2向右偏移两列。col-xs-8：占用 8 列的宽度。  */}
-          <div className="col-xs-offset-2 col-xs-8">
-            <div className="page-header">
-              <h2>React Router Demo</h2>
-            </div>
+const App = () => {
+  return (
+    <div>
+      <div className="row">
+        {/* offset-2 向右偏移两列。col-xs-8：占用 8 列的宽度。 */}
+        <div className="col-xs-offset-2 col-xs-8">
+          <div className="page-header">
+            <h2>React Router Demo</h2>
           </div>
         </div>
-        <div className="row">
-          <div className="col-xs-2 col-xs-offset-2">
-            <div className="list-group">
-              {/* 改造成 MyNavLink，这些写成<MyNavLink></MyNavLink>的形式是因为想把标签体内容以children的形式通过变量属性的方式带给MyNavLink那个封装体  */}
-              <MyNavLink to="/about">About </MyNavLink>
-              <MyNavLink to="/home">Home </MyNavLink>
-            </div>
+      </div>
+      <div className="row">
+        <div className="col-xs-2 col-xs-offset-2">
+          <div className="list-group">
+            {/* 改造成 MyNavLink */}
+            <MyNavLink to="/about">About</MyNavLink>
+            <MyNavLink to="/home">Home</MyNavLink>
           </div>
-          <div className="col-xs-6">
-            {/*panel 是 Bootstrap 的面板样式。  */}
-            <div className="panel">
-              <div className="panel-body">
-                {/* 注册路由 */}
-                <Routes>
-                  {/* 必须要设置一个默认的路径，添加 path="/" 或重定向到其他页面 */}
-                  <Route path="/" element={<Home />} />
-                  {/* 下面这行必须要，这样才可以继续找到其子路径 */}
-                  <Route path="/home/*" element={<Home />} />
-                  <Route path="/about" element={<About />} />
-                </Routes>
-              </div>
+        </div>
+        <div className="col-xs-6">
+          {/* panel 是 Bootstrap 的面板样式。 */}
+          <div className="panel">
+            <div className="panel-body">
+              {/* 注册路由 */}
+              <Routes>
+              <Route path="/React-myFirst-router" element={<Home />} />
+                <Route path="/React-myFirst-router/home/*" element={<Home />} />{/* 子路径 */}
+                <Route path="/React-myFirst-router/about" element={<About />} />
+              </Routes>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default App;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,34 +1,34 @@
-import React, { Component } from "react";
+import React from "react";
 import MyNavLink from "../../components/MyNavLink";
 import { Route, Routes, Outlet } from "react-router-dom";
 
 import News from "./News";
 import Message from "./Message";
 
-export default class Home extends Component {
-  render() {
-    return (
+const Home = () => {
+  return (
+    <div>
+      <h3>I'm home</h3>
       <div>
-        <h3>I'm home</h3>
-        <div>
-          <ul className="nav nav-tabs">
-            <li>
-              <MyNavLink to="/home/news">News</MyNavLink>
-            </li>
-            <li>
-              <MyNavLink to="/home/message">Message</MyNavLink>
-            </li>
-          </ul>
-          {/* 注册路由 */}
-          <Routes>
-            <Route path="news" element={<News />} />
-            {/* <Route path="message/*"></Route> */}
-            <Route path="/message/*" element={<Message />} />
-            {/* <Route path="message" element={<Message />} /> */}
-          </Routes>
-          <Outlet />
-        </div>
+        <ul className="nav nav-tabs">
+          <li>
+            <MyNavLink to="/home/news">News</MyNavLink>
+          </li>
+          <li>
+            <MyNavLink to="/home/message">Message</MyNavLink>
+          </li>
+        </ul>
+        {/* 注册路由 */}
+        <Routes>
+          <Route path="news" element={<News />} />
+          {/* <Route path="message/*"></Route> */}
+          <Route path="/message/*" element={<Message />} />
+          {/* <Route path="message" element={<Message />} /> */}
+        </Routes>
+        <Outlet />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Home;
